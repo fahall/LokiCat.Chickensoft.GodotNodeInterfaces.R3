eed79e3465dd2f75aefbaee7818f2387c135f3ce
Alex Hall <alex@alexhall.org>
2025-04-11 15:00:02 -0500

    Cleanup and rename

--- src/ObservableGenerator/ObservableExtensionGenerator.cs
+++ src/ObservableGenerator/ObservableExtensionGenerator.cs
@@ -21,90 +21,110 @@ namespace ObservableGenerator
 
         public void Execute(GeneratorExecutionContext context)
         {
-            var godotInterfaces = context.Compilation
-                .GetSymbolsWithName(name => name.StartsWith("I") && name.Contains("Godot"), SymbolFilter.Type)
-                .OfType<INamedTypeSymbol>()
-                .Where(t => t.TypeKind == TypeKind.Interface && t.ContainingNamespace.ToDisplayString().Contains("Chickensoft.GodotNodeInterfaces"))
-                .ToList();
-
-            foreach (var iface in godotInterfaces)
+            foreach (var nextInterface in GetInterfaces(context))
             {
-                var events = iface.GetMembers().OfType<IEventSymbol>()
-                    .Where(ev => ev.ContainingType.Equals(iface, SymbolEqualityComparer.Default))
-                    .ToList();
-
-                if (events.Count == 0)
-                    continue;
-
-                var sb = new StringBuilder();
-                var shortName = iface.Name.TrimStart('I');
-
-                sb.AppendLine("using System;");
-                sb.AppendLine("using System.Reactive;");
-                sb.AppendLine("using System.Reactive.Linq;");
-                sb.AppendLine("using System.Threading;");
-                sb.AppendLine($"public static class {shortName}Extensions");
-                sb.AppendLine("{");
-
-                foreach (var ev in events)
-                {
-                    if (ev.Type is not INamedTypeSymbol { TypeKind: TypeKind.Delegate } handler)
-                    {
-                        continue;
-                    }
-
-                    {
-                        var invoke = handler.DelegateInvokeMethod;
-                        if(invoke is null)
-                        {
-                            continue;
-                        }
-
-                        var parameters = invoke.Parameters;
-
-                        string returnType;
-                        string body;
-
-                        if (parameters is { Length: 1 })
-                        {
-                            returnType = $"Observable<{parameters[0].Type.ToDisplayString()}>";
-                            body =
-                                $"Observable.FromEvent<{handler.ToDisplayString()}, {parameters[0].Type.ToDisplayString()}>(\n" +
-                                $"        h => new {handler.Name}(h),\n" +
-                                $"        h => self.{ev.Name} += h,\n" +
-                                $"        h => self.{ev.Name} -= h,\n" +
-                                $"        cancellationToken\n    )";
-                        }
-                        else if (parameters is { Length: > 1 })
-                        {
-                            var tuple = string.Join(", ", parameters.Select(p => p.Type.ToDisplayString()));
-                            var args = string.Join(", ", parameters.Select(p => p.Name));
-                            body = $"Observable.FromEvent<{handler.ToDisplayString()}, ({tuple})>(\n" +
-                                   $"        h => new {handler.Name}(({args}) => h(({args}))),\n" +
-                                   $"        h => self.{ev.Name} += h,\n" +
-                                   $"        h => self.{ev.Name} -= h,\n" +
-                                   $"        cancellationToken\n    )";
-                            returnType = $"Observable<({tuple})>";
-                        }
-                        else
-                        {
-                            returnType = "Observable<Unit>";
-                            body = $"Observable.FromEvent(\n" +
-                                   $"        h => self.{ev.Name} += h,\n" +
-                                   $"        h => self.{ev.Name} -= h,\n" +
-                                   $"        cancellationToken\n    )";
-                        }
-
-                        sb.AppendLine(
-                            $"    public static {returnType} On{ev.Name}AsObservable(this {iface.Name} self, CancellationToken cancellationToken = default) =>");
-                        sb.AppendLine($"        {body};\n");
-                    }
-                }
-
-                sb.AppendLine("}");
-
-                context.AddSource($"{shortName}Extensions.g.cs", SourceText.From(sb.ToString(), Encoding.UTF8));
+                ExtendInterface(context, nextInterface);
             }
+        }
+
+        private static List<INamedTypeSymbol> GetInterfaces(GeneratorExecutionContext context)
+        {
+            var godotInterfaces = context.Compilation
+                                         .GetSymbolsWithName(name => name.StartsWith("I") && name.Contains("Godot"), SymbolFilter.Type)
+                                         .OfType<INamedTypeSymbol>()
+                                         .Where(t => t.TypeKind == TypeKind.Interface && t.ContainingNamespace.ToDisplayString().Contains("Chickensoft.GodotNodeInterfaces"))
+                                         .ToList();
+
+            return godotInterfaces;
+        }
+
+        private static void ExtendInterface(GeneratorExecutionContext context, INamedTypeSymbol anInterface)
+        {
+            var events = anInterface.GetMembers().OfType<IEventSymbol>()
+                              .Where(ev => ev.ContainingType.Equals(anInterface, SymbolEqualityComparer.Default))
+                              .ToList();
+
+            if (events.Count == 0)
+            {
+                return;
+            }
+
+            var sb = new StringBuilder();
+            var shortName = anInterface.Name.TrimStart('I');
+
+            sb.AppendLine("using System;");
+            sb.AppendLine("using System.Reactive;");
+            sb.AppendLine("using System.Reactive.Linq;");
+            sb.AppendLine("using System.Threading;");
+            sb.AppendLine($"public static class {shortName}Extensions");
+            sb.AppendLine("{");
+
+            BuildEventWrappers(events, sb, anInterface);
+
+            sb.AppendLine("}");
+
+            context.AddSource($"{shortName}Extensions.g.cs", SourceText.From(sb.ToString(), Encoding.UTF8));
+        }
+
+        private static void BuildEventWrappers(List<IEventSymbol> events, StringBuilder sb, INamedTypeSymbol iface)
+        {
+            foreach (var ev in events)
+            {
+                BuildEventWrapper(ev, sb, iface);
+            }
+        }
+
+        private static void BuildEventWrapper(IEventSymbol ev, StringBuilder sb, INamedTypeSymbol iface)
+        {
+            if (ev.Type is not INamedTypeSymbol { TypeKind: TypeKind.Delegate } handler)
+            {
+                return;
+            }
+
+            var invoke = handler.DelegateInvokeMethod;
+            if(invoke is null)
+            {
+                return;
+            }
+
+            var parameters = invoke.Parameters;
+
+            string returnType;
+            string body;
+
+            if (parameters is { Length: 1 })
+            {
+                returnType = $"Observable<{parameters[0].Type.ToDisplayString()}>";
+                body =
+                    $"Observable.FromEvent<{handler.ToDisplayString()}, {parameters[0].Type.ToDisplayString()}>(\n" +
+                    $"        h => new {handler.Name}(h),\n" +
+                    $"        h => self.{ev.Name} += h,\n" +
+                    $"        h => self.{ev.Name} -= h,\n" +
+                    $"        cancellationToken\n    )";
+            }
+            else if (parameters is { Length: > 1 })
+            {
+                var tuple = string.Join(", ", parameters.Select(p => p.Type.ToDisplayString()));
+                var args = string.Join(", ", parameters.Select(p => p.Name));
+                body = $"Observable.FromEvent<{handler.ToDisplayString()}, ({tuple})>(\n" +
+                       $"        h => new {handler.Name}(({args}) => h(({args}))),\n" +
+                       $"        h => self.{ev.Name} += h,\n" +
+                       $"        h => self.{ev.Name} -= h,\n" +
+                       $"        cancellationToken\n    )";
+                returnType = $"Observable<({tuple})>";
+            }
+            else
+            {
+                returnType = "Observable<Unit>";
+                body = $"Observable.FromEvent(\n" +
+                       $"        h => self.{ev.Name} += h,\n" +
+                       $"        h => self.{ev.Name} -= h,\n" +
+                       $"        cancellationToken\n    )";
+            }
+
+            sb.AppendLine(
+                $"    public static {returnType} On{ev.Name}AsObservable(this {iface.Name} self, CancellationToken cancellationToken = default) =>");
+            sb.AppendLine($"        {body};\n");
         }
     }
 }
