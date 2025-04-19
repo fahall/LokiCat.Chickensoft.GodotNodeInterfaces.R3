0eb41959857e6eb03bea1a0eec5c23854f29d0a0
Alex Hall <alex@alexhall.org>
2025-04-19 11:58:48 -0500

    Add signal to observable generation for custom signals

---
+++ src/LokiCat.GodotNodeInterfaces.Observables.ObservableGenerator/Features/SyntaxHelpers/Namespace.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace LokiCat.GodotNodeInterfaces.Observables.ObservableGenerator.Features.SyntaxHelpers;
+
+public class Namespace
+{
+    public static string GetNamespace(SyntaxNode node) =>
+        node.Ancestors().OfType<NamespaceDeclarationSyntax>().FirstOrDefault()?.Name.ToString()
+        ?? "Global";
+}
